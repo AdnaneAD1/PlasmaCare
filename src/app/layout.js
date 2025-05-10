10f79be17f5829ff58337d9f98e83812446f3deb
AdnaneAD1 <sidiamadouadnane4@gmail.com>
2025-05-10 20:07:12 +0200

    Update for resolve redirect error 7

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-// export const dynamic = 'force-dynamic';
-// export const fetchCache = 'force-no-store';
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 export const metadata = {
   title: 'KriolaCare',
   description: 'Plateforme de don de plasma',
